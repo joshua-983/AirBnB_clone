58bbb478773759220af534ce5eadd28fbcbe8968
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-11-14 21:32:11 +0000

    created console.py and models

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defininition for State class
+Attributes:
+    name(str): public class attribute
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class definition"""
+
+    name = ""
